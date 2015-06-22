94b0da9c231ae1d293d6aa0be9c3dcff14293bba
Vladimir Rodkin <mail@vovanr.com>
2015-06-22 11:19:14 +0300

    Lol

--- index.js
+++ index.js
@@ -16,18 +16,80 @@ define([
     /**
      * @param {Object} o Options
      * @param {jQuery} o.$scroll Scroller block
+     * @param {jQuery} o.$buttons Scroller buttons
      * @constructor
      * @alias module:ScrollButtons
      */
     ScrollButtons = function (o) {
         this._$scroll = o.$scroll;
+        this._$buttons = o.$buttons;
+
+        this._amount = '';
+
+        this._init();
     };
 
     ScrollButtons.prototype = {
         /**
+         * Initialize
+         *
+         * @private
+         */
+        _init: function () {
+            this._bindControls();
+        },
+
+        /**
+         * Bindings
+         *
+         * @private
+         */
+        _bindControls: function () {
+            var _this = this;
+            this._$buttons.hover(function () {
+                var $this = $(this);
+                _this._amount = $this.data('scroll-amount');
+                _this._scroll();
+            }, function () {
+                _this._amount = '';
+            });
+        },
+
+        /**
+         * @public
+         */
+        show: function () {
+            this._$buttons.show();
+        },
+
+        /**
+         * @public
+         */
+        hide: function () {
+            this._$buttons.hide();
+        },
+
+        /**
+         * Прокрутка списка экскурсий
+         *
+         * @private
+         */
+        _scroll: function () {
+            var _this = this;
+            this._$scroll.animate({
+                scrollTop: _this._amount,
+            }, 100, 'linear', function () {
+                if (_this._amount !== '') {
+                    _this._scroll();
+                }
+            });
+        },
+
+        /**
          * @public
          */
         destroy: function () {
+            this._amount = '';
             this._$scroll = null;
         },
     };
--- test/index-test.js
+++ test/index-test.js
@@ -27,6 +27,7 @@ requirejs([
                 o || {},
                 {
                     $scroll: $fixtures.find('.js-list__scroll'),
+                    $buttons: $fixtures.find('.js-scroll-buttons__button'),
                 }
             );
 
@@ -57,7 +58,7 @@ requirejs([
         });
 
         afterEach(function () {
-            // setScrollerTop(0);
+            setScrollerTop(0);
         });
 
         describe('constructor', function () {
@@ -65,12 +66,94 @@ requirejs([
                 var m = module();
                 assert.ok(m._$scroll.length);
             });
+
+            it('should have `$buttons` option', function () {
+                var m = module();
+                assert.ok(m._$buttons.length);
+            });
         });
 
         describe('_init', function () {
+            it('should fire `_bindControls`', function () {
+                var m = module();
+                sinon.stub(m, '_bindControls');
+                assert.notOk(m._bindControls.called);
+                m._init();
+                assert.ok(m._bindControls.calledOnce);
+            });
         });
 
         describe('_bindControls', function () {
+            it('should bind buttons', function () {
+                var m = module();
+                sinon.stub(m, '_scroll');
+                var $buttons = $('#fixtures .js-scroll-buttons__button');
+                $buttons.first().trigger('mouseover');
+                assert.ok(m._scroll.called);
+                $buttons.first().trigger('mouseout');
+            });
+
+            it('should bind scroll up button', function (done) {
+                var m = module();
+                setScrollerTop(1000);
+                var $button = $('#fixtures .scroll-buttons__button_direction_up');
+                var old = getScrollerTop();
+                assert.ok(old > 0);
+                $button.trigger('mouseover');
+                setTimeout(function () {
+                    assert.ok(old > getScrollerTop());
+                    $button.trigger('mouseout');
+                    done();
+                }, 100);
+            });
+
+            it('should bind scroll down button', function (done) {
+                var m = module();
+                setScrollerTop(0);
+                var $button = $('#fixtures .scroll-buttons__button_direction_down');
+                assert.equal(getScrollerTop(), 0);
+                $button.trigger('mouseover');
+                setTimeout(function () {
+                    assert.ok(getScrollerTop() > 0);
+                    $button.trigger('mouseout');
+                    done();
+                }, 100);
+            });
+        });
+
+        describe('#show', function () {
+            it('should show scroll buttons', function () {
+                var m = module();
+                var $buttons = $('#fixtures .js-scroll-buttons__button');
+                assert.equal($buttons.css('display'), 'none');
+                m.show();
+                assert.equal($buttons.css('display'), 'block');
+            });
+        });
+
+        describe('#hide', function () {
+            it('should hide scroll buttons', function () {
+                var m = module();
+                var $buttons = $('#fixtures .js-scroll-buttons__button');
+                $buttons.show();
+                assert.equal($buttons.css('display'), 'block');
+                m.hide();
+                assert.equal($buttons.css('display'), 'none');
+            });
+        });
+
+        describe('_scroll', function () {
+            it('should scroll list on scroll amount option', function (done) {
+                var m = module();
+                m._amount = '+=50';
+                assert.equal(getScrollerTop(), 0);
+                m._scroll();
+                setTimeout(function () {
+                    assert.notEqual(getScrollerTop(), 0);
+                    m._amount = '';
+                    done();
+                }, 100);
+            });
         });
 
         describe('#destroy', function () {
@@ -79,6 +162,13 @@ requirejs([
                 assert.ok(m._$scroll.length);
                 m.destroy();
                 assert.isNull(m._$scroll);
+            });
+
+            it('should reset `_amount`', function () {
+                var m = module();
+                m._amount = '+50';
+                m.destroy();
+                assert.equal(m._amount, '');
             });
         });
     });
